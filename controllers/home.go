da00edb9b98c3322f2ae10f1f17349102bce76fd
luca <luca@lucadeMacBook-Pro.local>
2026-04-06 15:15:54 +0800

    广告位

--- controllers/home.go
+++ controllers/home.go
@@ -10,10 +10,16 @@ import (
 func HomeHandler(c *gin.Context) {
 	// 获取首页轮播广告
 	heroAds, _ := models.GetAdsByPositionCode("home")
+	// 获取国际销售版块广告
+	internationalSalesAds, _ := models.GetAdsByPositionCode("home-international-sales")
+	// 获取关于版块广告
+	aboutAds, _ := models.GetAdsByPositionCode("home-about")
 
 	c.HTML(200, "index.html", gin.H{
-		"title":   "Premium Fasteners & Bolts Supplier - High Quality Industrial Fasteners",
-		"heroAds": heroAds,
-		"active":  "home",
+		"title":                "Premium Fasteners & Bolts Supplier - High Quality Industrial Fasteners",
+		"heroAds":              heroAds,
+		"internationalSalesAd": internationalSalesAds,
+		"aboutAd":              aboutAds,
+		"active":               "home",
 	})
 }
